801e57f928e14108ab91053de3dba64c34fcbbd6
jyclam <johnlamyc@gamil.com>
2018-03-08 12:30:59 -0800

    Refactor ConnectToEmulator.specs

--- client/crud/test/web-functional/chimp.js
+++ client/crud/test/web-functional/chimp.js
@@ -1,5 +1,11 @@
 module.exports = {
-    path: 'web-functional/features'
+    path: 'web-functional/features',
+    webdriverio: {
+        waitforTimeout: 5000
+    },
+    mochaConfig: {
+        timeout: 60 * 1000 * 5
+    }
 };
 
 
--- client/crud/test/web-functional/features/ConnectToEmulator.specs.js
+++ client/crud/test/web-functional/features/ConnectToEmulator.specs.js
@@ -1,47 +1,34 @@
-import {start, shutdown, setData} from '../emulator/Emulator';
+import {start, shutdown, setData, setMaxNodes, getData} from '../emulator/Emulator';
+import {nodes} from '../emulator/NodeStore';
 import {findComponentsTest} from "react-functional-test";
 
 describe('Emulator connections', () => {
 
-    it('emulator should not have ')
+    before(() => {
+        start(8100);
+    });
 
-    it('should be able to connect to the emulator', () => {
-
-        start();
-
+    beforeEach(() => {
+        wrapAsync(() => Promise.all(shutdown()))();
+        setMaxNodes(1);
+        browser.waitUntil(() => nodes.keys().length === 1);
         browser.url('http://localhost:8200/?test');
-
         browser.waitForExist('button=Go');
         browser.element('button=Go').click();
+    });
 
+    it('should be able to connect to the emulator', () => {
         browser.waitUntil(() => findComponentsTest('KeyList').length > 0)
-
-        shutdown();
-
     });
 
 
-    it('should be able to set data', () => {
-
+    it('@watch should be able to set data', () => {
         const data = {
             text: [1, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 44, 32, 116, 104, 105, 115, 32, 105, 115, 32, 115, 111, 109, 101, 32, 112, 108, 97, 105, 110, 32, 116, 101, 120, 116, 46]
         };
 
-        start();
         setData(data);
-
-
-        browser.url('http://localhost:8200/?test');
-
-        browser.waitForExist('button=Go');
-        browser.element('button=Go').click();
-
-
         browser.waitUntil(() =>
             findComponentsTest('KeyListItem').some(comp => comp.props.keyname === 'text'), 10000);
-
-        shutdown();
-
     });
-
 });

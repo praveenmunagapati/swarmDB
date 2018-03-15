0a64c63e587bb52ac03c3d3be3ce998166c60559
Monty Thibault <montythibault@gmail.com>
2018-03-15 10:12:54 -0700

    Refactoring notes from Monty

--- client/emulator/CommandProcessors.js
+++ client/emulator/CommandProcessors.js
@@ -8,5 +8,9 @@ module.exports = {
         connection.send(JSON.stringify({cmd: 'updateNodes', data: getAllNodesInfo()})),
     read,
     update,
+
+    // delete
+    // aggregate
+
     requestKeyList
 };

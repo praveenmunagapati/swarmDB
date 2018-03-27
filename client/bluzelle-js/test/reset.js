b6fe167c139a17cc9b9e2c74652a35f6c17c5b4b
Monty Thibault <montythibault@gmail.com>
2018-03-27 13:08:38 -0700

    Added reset.js

---
+++ client/bluzelle-js/test/reset.js
@@ -0,0 +1,42 @@
+
+const resetInNode = () => 
+	require('emulator/Emulator').reset();
+
+
+const resetInBrowser = () => new Promise(resolve => {
+
+	const ws = new WebSocket('ws://localhost:8101');
+	
+	ws.onopen(() => {
+
+		ws.send('reset');
+
+	});
+
+	ws.onmessage(() => {
+
+		ws.close();
+		resolve();
+
+	});
+
+});
+
+
+module.exports = {
+
+	reset: () => {
+
+		if(typeof window === 'undefined') {
+
+			return resetInNode();
+
+		} else {
+
+			return resetInBrowser();
+
+		}
+
+	}
+
+};

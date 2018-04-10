eee98f94081258702840e60d9ac254ff6a1c4cb9
jyclam <johnlamyc@gamil.com>
2018-04-09 19:26:36 -0700

    Add test for js-library reset in node

--- client/bluzelle-js/test/0-beforehook.test.js
+++ client/bluzelle-js/test/0-beforehook.test.js
@@ -1,5 +1,32 @@
 const reset = require('./reset');
+const communication = require('../communication');
+const assert = require('assert');
 
+
+// Global reset for whole testing suite
 beforeEach(reset);
 
-it('should be able to reset', () => {});
+describe('reset', () => {
+
+    describe('in node environment', () => {
+
+        beforeEach(async () =>{
+            await communication.connect('ws://localhost:8100', '2222');
+        });
+
+        afterEach(() => {
+            communication.disconnect();
+        });
+
+        it('can add a key', async () => {
+            await communication.update('myKey', 'abc');
+            assert(await communication.has('myKey'));
+            assert(!await communication.has('someOtherKey'));
+        });
+
+        it('should not have key from previous test', async () => {
+            assert(!await communication.has('myKey'));
+        });
+    });
+
+});

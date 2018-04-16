03ee226aaed8477adc127e65cccb0d336962e881
jyclam <johnlamyc@gamil.com>
2018-04-15 23:49:38 -0700

    Functional multiclient test

--- client/bluzelle-js/test/multiclient.test.js
+++ client/bluzelle-js/test/multiclient.test.js
@@ -15,26 +15,28 @@ describe('multi-client bluzelle api', () => {
 
     beforeEach(reset);
 
-    beforeEach( async () => {
-        await api1.connect('ws://localhost:8100', '7777');
+    beforeEach(async () => {
+        await api1.connect('ws://localhost:8100', '9999');
         api1.setup();
-    });
 
-     beforeEach( async () => {
         await api2.connect('ws://localhost:8100', '8888');
         api2.setup();
     });
 
+    afterEach(() => {
+        api1.disconnect();
+        api2.disconnect();
+    });
+
+
     it('api1 should be able to create and read number fields', async () => {
         await api1.update('myKey', 123);
         assert(await api1.read('myKey') === 123);
-
     });
 
     it('api2 should be able to create and read number fields', async () => {
         await api2.update('myKey', 123);
         assert(await api2.read('myKey') === 123);
-
     });
 
 });
